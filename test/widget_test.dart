de5185cfcad37cfb429f68f18b775fe315a90e4c
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-11-12 15:46:16 +0300

    test blank app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:pharmacy_warehouse_store_mobile/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PhrarmacyWarehouseStoreMobile());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
