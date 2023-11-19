1e011f7d781e9ffe8076f4d977f25b168b0c1928
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-11-20 01:42:40 +0300

    create initial product details screen

---
+++ lib/src/view/screens/product_details_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+    );
+  }
+}
