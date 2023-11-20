eea9169194017fb138dde3e714b7f598520344e7
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-11-21 00:36:29 +0300

    add api implementation

--- lib/src/services/api.dart
+++ lib/src/services/api.dart
@@ -1,1 +1,81 @@
-class Api {}
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class Api {
+  Future<dynamic> get({required String url, @required String? token}) async {
+    Map<String, String> headers = {};
+
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response = await http.get(Uri.parse(url), headers: headers);
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception(response.statusCode);
+    }
+  }
+
+  Future<dynamic> post(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response =
+        await http.post(Uri.parse(url), body: body, headers: headers);
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+
+      return data;
+    } else {
+      throw Exception(response.statusCode);
+    }
+  }
+
+  Future<dynamic> put(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    headers.addAll({'Content-Type': 'application/x-www-form-urlencoded'});
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+
+    http.Response response =
+        await http.put(Uri.parse(url), body: body, headers: headers);
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception(response.statusCode);
+    }
+  }
+
+  Future<dynamic> delete(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response =
+        await http.delete(Uri.parse(url), body: body, headers: headers);
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+
+      return data;
+    } else {
+      throw Exception(response.statusCode);
+    }
+  }
+}
