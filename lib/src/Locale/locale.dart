8227c43a637029fa63aed31c019bc9b541bdc988
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-11-21 01:21:27 +0300

    organise local file

--- lib/src/Locale/locale.dart
+++ lib/src/Locale/locale.dart
@@ -4,7 +4,10 @@ class AppLocale implements Translations {
   @override
   Map<String, Map<String, String>> get keys => {
         "ar": {
+          // start
           "welcomeMessage": "أهلاً بعودتك",
+
+          // auth
           "enterCredentials": "من فضلك أدخل بياناتك الائتمانية",
           "signIn": "تسجيل الدخول",
           "userNumber": "رقم المستخدم",
@@ -22,10 +25,14 @@ class AppLocale implements Translations {
           "enterValidNumber": "من فضلك ادخل رقماً صالحاً",
           "passwordShouldBe8": "يجب أن تتألف كلمة المرور من 8 محارف على الأقل",
           "passwordsDontMatch": "كلمتا المرور لا تتطابقان",
+          "letsCreateAnAccount": "لننشئ حساباً من أجلك",
+
+          // snack bar messages
           "signedInSuccess": "تم تسجيل الدخول بنجاح !",
           "registerSuccess": "تم تسجيل الحساب بنجاح !",
-          "letsCreateAnAccount": "لننشئ حساباً من أجلك",
           "close": "أغلق",
+
+          // botton nav bar
           "home": "الرئيسية",
           "search": "البحث",
           "cart": "سلة المشتريات",
@@ -33,7 +40,10 @@ class AppLocale implements Translations {
           "favourite": "المفضلة",
         },
         "en": {
+          //start
           "welcomeMessage": "Welcome back",
+
+          // auth
           "enterCredentials": "Please enter your credentials to Login",
           "signIn": "Sign in",
           "userNumber": "User Number",
@@ -51,10 +61,14 @@ class AppLocale implements Translations {
           "enterValidNumber": "Please enter a valid number",
           "passwordShouldBe8": "Password should be at least 8 characters",
           "passwordsDontMatch": "Passwords don't match",
+          "letsCreateAnAccount": "Let's create an account for you",
+
+          // snack bar messages
           "signedInSuccess": "Signed in successfully !",
           "registerSuccess": "Signed up successfully! ",
-          "letsCreateAnAccount": "Let's create an account for you",
           "close": "Close",
+
+          // botton nav bar
           "home": "Home",
           "search": "Search",
           "cart": "Cart",
