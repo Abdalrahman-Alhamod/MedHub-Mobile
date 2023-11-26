36342ec7e1ebe58cfd24743eaa924365dd42ea2c
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-11-27 02:08:25 +0300

    create porudct list screen

--- lib/src/view/screens/navigation bar/products_list_screen.dart
+++ lib/src/view/screens/navigation bar/products_list_screen.dart
@@ -1,13 +1,184 @@
 import 'package:flutter/material.dart';
-import 'package:pharmacy_warehouse_store_mobile/core/constants/app_colors.dart';
+import 'package:get/get.dart';
+import 'package:pharmacy_warehouse_store_mobile/core/assets/app_images.dart';
+import 'package:pharmacy_warehouse_store_mobile/core/data/app_data.dart';
+import 'package:pharmacy_warehouse_store_mobile/src/view/widgets/custome_text_field.dart';
+import 'package:pharmacy_warehouse_store_mobile/src/view/widgets/product_card.dart';
 
 class ProductsListScreen extends StatelessWidget {
-  const ProductsListScreen({super.key});
+  const ProductsListScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: AppColors.primaryColor,
+    final theme = context.theme;
+    return Scaffold(
+      body: Stack(
+        children: [
+          GestureDetector(
+            onTap: () {
+              // This will unfocus the keyboard
+              FocusScope.of(context).unfocus();
+            },
+            child: Container(
+              color: Colors
+                  .transparent, // Use a transparent container to capture taps
+            ),
+          ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 8, right: 16, left: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomeTextField(
+                    obscureText: false,
+                    hintText: "searchFor".tr,
+                    onChanged: (value) {},
+                    validator: null,
+                    keyboardType: TextInputType.text,
+                    prefixIcon: Icons.search,
+                    onTap: () {},
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const _AdsView(),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "categories".tr,
+                    style: theme.textTheme.titleLarge,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  _CategoriesCardsView(theme: theme),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "mostPopular".tr,
+                    style: theme.textTheme.titleLarge,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  _ProductCardsView(theme: theme),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "recentlyAdd".tr,
+                    style: theme.textTheme.titleLarge,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  _ProductCardsView(theme: theme),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ProductCardsView extends StatelessWidget {
+  const _ProductCardsView({
+    required this.theme,
+  });
+
+  final ThemeData theme;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 230,
+      width: double.infinity,
+      child: ListView.builder(
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: ProductCard(
+              theme: theme,
+              product: AppData.products[0],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _CategoriesCardsView extends StatelessWidget {
+  const _CategoriesCardsView({
+    required this.theme,
+  });
+
+  final ThemeData theme;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      width: double.infinity,
+      child: ListView.builder(
+        itemCount: AppData.categories.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Container(
+            margin: const EdgeInsets.only(right: 8),
+            decoration: BoxDecoration(
+              color: Colors.blueGrey,
+              borderRadius: BorderRadius.circular(8),
+              image: const DecorationImage(
+                  image: AssetImage(AppImages.categoryWallpaper),
+                  fit: BoxFit.cover),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Center(
+                  child: Text(AppData.categories[index].name,
+                      style: theme.textTheme.titleMedium!.copyWith(
+                          color: const Color.fromARGB(255, 34, 77, 112)))),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _AdsView extends StatelessWidget {
+  const _AdsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      width: double.infinity,
+      child: ListView.builder(
+        itemCount: AppImages.ads.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Container(
+            margin: const EdgeInsets.only(right: 8),
+            width: 220,
+            decoration: BoxDecoration(
+              color: Colors.blueGrey,
+              borderRadius: BorderRadius.circular(8),
+              image: DecorationImage(
+                  image: AssetImage(AppImages.ads[index]), fit: BoxFit.cover),
+            ),
+          );
+        },
+      ),
     );
   }
 }
