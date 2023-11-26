2b90f79184314bc714c18d365c1958b09443fe4a
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-11-27 02:04:26 +0300

    test product controller

--- lib/src/controller/product_controller.dart
+++ lib/src/controller/product_controller.dart
@@ -3,15 +3,24 @@ import 'package:pharmacy_warehouse_store_mobile/core/data/app_data.dart';
 import 'package:pharmacy_warehouse_store_mobile/src/model/product.dart';
 
 class ProductController extends GetxController {
-  RxList<Product> products = AppData.products.obs;
-
+  final products = AppData.products.obs;
+  var loading = false.obs;
+  
   void isFavouriate({required int productId}) {
     for (var product in products) {
       if (product.id == productId) {
         product.isFavourate = !product.isFavourate;
-        update();
         break;
       }
     }
+  }
+
+  Product? getProductById({required int productId}) {
+    for (var product in products) {
+      if (product.id == productId) {
+        return product;
+      }
+    }
+    return null;
   }
 }
