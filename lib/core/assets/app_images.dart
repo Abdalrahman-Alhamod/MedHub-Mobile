0577d54b94d4b5647aeae3c8ca06a995e15ec00b
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-11-27 02:03:31 +0300

    add new images path to AppImages

--- lib/core/assets/app_images.dart
+++ lib/core/assets/app_images.dart
@@ -1,4 +1,12 @@
 class AppImages {
   const AppImages._();
-  static const startWallpaper = 'assets/images/wallpaper.jpg';
+  static const startWallpaper = 'assets/images/start.jpg';
+  static const categoryWallpaper = 'assets/images/wallpaper.jpg';
+  static const ads = [
+    'assets/images/ads1.jpeg',
+    'assets/images/ads2.jpg',
+    'assets/images/ads3.jpg',
+    'assets/images/ads4.jpeg',
+    'assets/images/ads5.jpg',
+  ];
 }
