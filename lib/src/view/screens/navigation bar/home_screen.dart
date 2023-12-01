97cb08094ea2caa01861e0cce7fdf9e3506a6f77
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-12-01 04:16:41 +0300

    implement bottom nav bar bloc in home page

--- lib/src/view/screens/navigation bar/home_screen.dart
+++ lib/src/view/screens/navigation bar/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 import 'package:pharmacy_warehouse_store_mobile/core/constants/app_colors.dart';
+import 'package:pharmacy_warehouse_store_mobile/src/Cubits/BottomNavBarCubit/bottom_nav_bar_cubit.dart';
 import 'package:pharmacy_warehouse_store_mobile/src/controller/user_controller.dart';
 import 'package:pharmacy_warehouse_store_mobile/src/view/screens/navigation bar/cart_screen.dart';
 import 'package:pharmacy_warehouse_store_mobile/src/view/screens/navigation bar/favourite_screen.dart';
@@ -25,102 +27,105 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
-  int _index = 1;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.primaryColor,
-        title: const Text(
-          'MedHub',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () async {
-            UserController().logout();
-          },
-          icon: const Icon(
-            Icons.logout,
-            size: 32,
-            color: AppColors.secondaryColor,
-          ),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.notifications,
-              size: 32,
-              color: Colors.yellow.shade700,
+    return BlocBuilder<BottomNavBarCubit, BottomNavBarState>(
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: AppColors.primaryColor,
+            title: const Text(
+              'MedHub',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-        ],
-      ),
-      body: SizedBox.expand(
-        child: HomeScreen.screen[_index],
-      ),
-      bottomNavigationBar: BottomNavyBar(
-        selectedIndex: _index,
-        containerHeight: 70,
-        onItemSelected: (index) {
-          setState(() {
-            _index = index;
-          });
-        },
-        items: <BottomNavyBarItem>[
-          BottomNavyBarItem(
-            title: Text("favourite".tr),
-            icon: const Icon(
-              Icons.favorite,
-              size: 32,
+            centerTitle: true,
+            leading: IconButton(
+              onPressed: () async {
+                UserController().logout();
+              },
+              icon: const Icon(
+                Icons.logout,
+                size: 32,
+                color: AppColors.secondaryColor,
+              ),
             ),
-            activeColor: AppColors.primaryColor,
-            inactiveColor: Colors.grey,
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.notifications,
+                  size: 32,
+                  color: Colors.yellow.shade700,
+                ),
+              ),
+            ],
           ),
-          BottomNavyBarItem(
-            title: Text("search".tr),
-            icon: const Icon(
-              Icons.search,
-              size: 32,
-            ),
-            activeColor: AppColors.primaryColor,
-            inactiveColor: Colors.grey,
+          body: SizedBox.expand(
+            child: HomeScreen
+                .screen[BlocProvider.of<BottomNavBarCubit>(context).index],
           ),
-          BottomNavyBarItem(
-            title: Text("home".tr),
-            icon: const Icon(
-              Icons.home,
-              size: 32,
-            ),
-            activeColor: AppColors.primaryColor,
-            inactiveColor: Colors.grey,
+          bottomNavigationBar: BottomNavyBar(
+            selectedIndex: BlocProvider.of<BottomNavBarCubit>(context).index,
+            containerHeight: 70,
+            onItemSelected: (index) {
+              BlocProvider.of<BottomNavBarCubit>(context)
+                  .navigate(index: index);
+            },
+            items: <BottomNavyBarItem>[
+              BottomNavyBarItem(
+                title: Text("favourite".tr),
+                icon: const Icon(
+                  Icons.favorite,
+                  size: 32,
+                ),
+                activeColor: AppColors.primaryColor,
+                inactiveColor: Colors.grey,
+              ),
+              BottomNavyBarItem(
+                title: Text("search".tr),
+                icon: const Icon(
+                  Icons.search,
+                  size: 32,
+                ),
+                activeColor: AppColors.primaryColor,
+                inactiveColor: Colors.grey,
+              ),
+              BottomNavyBarItem(
+                title: Text("home".tr),
+                icon: const Icon(
+                  Icons.home,
+                  size: 32,
+                ),
+                activeColor: AppColors.primaryColor,
+                inactiveColor: Colors.grey,
+              ),
+              BottomNavyBarItem(
+                title: Text(
+                  "cart".tr,
+                ),
+                icon: const Icon(
+                  Icons.shopping_cart,
+                  size: 32,
+                ),
+                activeColor: AppColors.primaryColor,
+                inactiveColor: Colors.grey,
+              ),
+              BottomNavyBarItem(
+                title: Text("orders".tr),
+                icon: const Icon(
+                  Icons.receipt,
+                  size: 32,
+                ),
+                activeColor: AppColors.primaryColor,
+                inactiveColor: Colors.grey,
+              ),
+            ],
           ),
-          BottomNavyBarItem(
-            title: Text(
-              "cart".tr,
-            ),
-            icon: const Icon(
-              Icons.shopping_cart,
-              size: 32,
-            ),
-            activeColor: AppColors.primaryColor,
-            inactiveColor: Colors.grey,
-          ),
-          BottomNavyBarItem(
-            title: Text("orders".tr),
-            icon: const Icon(
-              Icons.receipt,
-              size: 32,
-            ),
-            activeColor: AppColors.primaryColor,
-            inactiveColor: Colors.grey,
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
