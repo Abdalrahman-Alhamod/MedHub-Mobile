d0c63f058144888e8a3248e19a6fe61c2f7ad74c
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-12-01 03:16:17 +0300

    create products cubit and implement its states

---
+++ lib/src/Cubits/ProductsCubit/products_cubit.dart
@@ -0,0 +1,34 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pharmacy_warehouse_store_mobile/core/data/app_data.dart';
+import 'package:pharmacy_warehouse_store_mobile/src/model/category.dart';
+import 'package:pharmacy_warehouse_store_mobile/src/model/product.dart';
+
+part 'products_state.dart';
+
+class ProductsCubit extends Cubit<ProductsState> {
+  ProductsCubit() : super(ProductsInitial());
+  String? searchBarContent;
+  Category? choosenCategory;
+
+  Future<void> search() async {
+    log("Chooosen category name : ${choosenCategory!.name}");
+    log("Search bar content : $searchBarContent");
+    try {
+      emit(ProductsFetchLoading());
+      await Future.delayed(const Duration(seconds: 2));
+      List<Product> products = AppData.filteredProducts[choosenCategory!.id];
+      if (products.isEmpty) {
+        emit(ProductsNotFound());
+      } else {
+        emit(ProductsFetchSuccess(products: products));
+      }
+    } on DioException catch (exception) {
+      emit(NetworkFailure(errorMessage: exception.message.toString()));
+    } catch (e) {
+      emit(ProductsFetchFailure(errorMessage: e.toString()));
+    }
+  }
+}
---
+++ lib/src/Cubits/ProductsCubit/products_state.dart
@@ -0,0 +1,24 @@
+part of 'products_cubit.dart';
+
+final class ProductsState {}
+
+final class ProductsInitial extends ProductsState {}
+
+final class ProductsFetchLoading extends ProductsState {}
+
+final class ProductsFetchSuccess extends ProductsState {
+  List<Product> products;
+  ProductsFetchSuccess({required this.products});
+}
+
+final class ProductsFetchFailure extends ProductsState {
+  String errorMessage;
+  ProductsFetchFailure({required this.errorMessage});
+}
+
+final class ProductsNotFound extends ProductsState {}
+
+final class NetworkFailure extends ProductsState {
+  String errorMessage;
+  NetworkFailure({required this.errorMessage});
+}
