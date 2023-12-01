58c0bfe11ca9ceca2be341073746c22a259dd616
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-12-01 04:15:52 +0300

    create bottom navigation bar cubit

---
+++ lib/src/Cubits/BottomNavBarCubit/bottom_nav_bar_cubit.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'bottom_nav_bar_state.dart';
+
+class BottomNavBarCubit extends Cubit<BottomNavBarState> {
+  BottomNavBarCubit() : super(BottomNavBarInitial());
+  int index = 2;
+  void navigate({required int index}) {
+    this.index = index;
+    emit(PageNavigation());
+  }
+}
---
+++ lib/src/Cubits/BottomNavBarCubit/bottom_nav_bar_state.dart
@@ -0,0 +1,9 @@
+part of 'bottom_nav_bar_cubit.dart';
+
+final class BottomNavBarState {}
+
+final class BottomNavBarInitial extends BottomNavBarState {
+}
+
+final class PageNavigation extends BottomNavBarState {
+}
