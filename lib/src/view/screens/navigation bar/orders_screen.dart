c4ca5a2c41d273dd17c9e87d07e69d9fd4538b6a
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-12-03 01:41:04 +0300

    add image to order screen

--- lib/src/view/screens/navigation bar/orders_screen.dart
+++ lib/src/view/screens/navigation bar/orders_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pharmacy_warehouse_store_mobile/core/assets/app_images.dart';
+import 'package:pharmacy_warehouse_store_mobile/src/view/widgets/show_image.dart';
 
 class OrdersScreen extends StatelessWidget {
   const OrdersScreen({super.key});
@@ -6,7 +8,13 @@ class OrdersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      backgroundColor: Colors.purple,
+      body: Center(
+        child: ShowImage(
+          imagePath: AppImages.emptyOrders,
+          height: 600,
+          width: 600,
+        ),
+      ),
     );
   }
 }
