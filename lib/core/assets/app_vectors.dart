df150b82457e50d26df19a659eee46d6fde5e61b
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-12-03 01:29:48 +0300

    create all svgs list

--- lib/core/assets/app_vectors.dart
+++ lib/core/assets/app_vectors.dart
@@ -2,4 +2,8 @@ class AppVector {
   const AppVector._();
   static const container = 'assets/SVGs/container.svg';
   static const backArrowIcon = 'assets/SVGs/back_arrow.svg';
+  static const allSVGs = [
+    container,
+    backArrowIcon,
+  ];
 }
