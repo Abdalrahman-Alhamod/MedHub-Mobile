4b04df8aec6e98a7225702913dbee0c704cf9de3
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-12-03 01:30:33 +0300

    modify testing app data due to classes changes

--- lib/core/data/app_data.dart
+++ lib/core/data/app_data.dart
@@ -7,24 +7,24 @@ import 'package:pharmacy_warehouse_store_mobile/src/model/product.dart';
 class Products {
   static final _format = DateFormat('yyyy/MM/dd', Intl.getCurrentLocale());
   static final _date = _format.format(DateTime.now()).toString();
+  // ignore: non_constant_identifier_names
   static final Product Amoxil = Product(
     id: 0,
-    type: ProductType.Antibiotics,
+    category: AppData.categories[1],
     name: "Amoxil".tr,
     scientificName: "Amoxicillin".tr,
     brand: "GlaxoSmithKline".tr,
     description: "desc".tr,
     expirationDate: _date,
     price: 17500,
-    popularity: 520,
     image: AppProductsImages.amoxil,
     inStock: 0,
     isFavourate: false,
-    discount: 50,
   );
+  // ignore: non_constant_identifier_names
   static final Product Excedrin = Product(
     id: 1,
-    type: ProductType.PainAndRelief,
+    category: AppData.categories[8],
     name: "Excedrin",
     scientificName: "Acetaminophen",
     brand: "GlaxoSmithKline",
@@ -32,15 +32,14 @@ class Products {
         "Excedrin is a brand of over-the-counter headache pain reliever that typically combines acetaminophen, aspirin, and caffeine to provide relief from various types of headaches, including tension headaches and migraines",
     expirationDate: _date,
     price: 22300,
-    popularity: 520,
     image: AppProductsImages.excedrin,
-    inStock: 64,
+    inStock: 3,
     isFavourate: false,
-    discount: 50,
   );
+  // ignore: non_constant_identifier_names
   static final Product Benadryl = Product(
     id: 3,
-    type: ProductType.AllergyMedications,
+    category: AppData.categories[2],
     name: "Benadryl",
     scientificName: "Diphenhydramine",
     brand: "Johnson & Johnson",
@@ -48,15 +47,14 @@ class Products {
         "Benadryl is an antihistamine medication that alleviates symptoms of allergies, such as sneezing, runny nose, itching, and watery eyes, by blocking the effects of histamine, a natural substance produced by the body in response to allergens",
     expirationDate: _date,
     price: 26500,
-    popularity: 520,
     image: AppProductsImages.benadryl,
     inStock: 96,
     isFavourate: false,
-    discount: 50,
   );
+  // ignore: non_constant_identifier_names
   static final Product PeptoBismol = Product(
     id: 4,
-    type: ProductType.DigestiveHealth,
+    category: AppData.categories[7],
     name: "Pepto-Bismol",
     scientificName: "Bismuth subsalicylate",
     brand: "Procter & Gamble",
@@ -64,15 +62,14 @@ class Products {
         "Pepto-Bismol is an over-the-counter medication that provides relief from digestive discomfort, including indigestion, upset stomach, heartburn, nausea, and diarrhea, by coating and soothing the gastrointestinal tract",
     expirationDate: _date,
     price: 8500,
-    popularity: 520,
     image: AppProductsImages.peptoBismol,
     inStock: 123,
     isFavourate: false,
-    discount: 50,
   );
+  // ignore: non_constant_identifier_names
   static final Product Robitussin = Product(
     id: 2,
-    type: ProductType.ColdAndFlu,
+    category: AppData.categories[4],
     name: "Robitussin",
     scientificName: "Dextromethorphan",
     brand: "Pfizer",
@@ -80,15 +77,14 @@ class Products {
         "Robitussin is a cough medicine that helps relieve cough symptoms by thinning and loosening mucus in the airways, making it easier to cough out and providing relief from chest congestion",
     expirationDate: _date,
     price: 13200,
-    popularity: 520,
     image: AppProductsImages.robitussin,
     inStock: 32,
     isFavourate: false,
-    discount: 50,
   );
+  // ignore: non_constant_identifier_names
   static final Product Lipitor = Product(
     id: 5,
-    type: ProductType.CardiovascularMedications,
+    category: AppData.categories[3],
     name: "Lipitor",
     scientificName: "Atorvastatin",
     brand: "Pfizer",
@@ -96,15 +92,14 @@ class Products {
         "Lipitor is a prescription medication used to lower cholesterol levels and reduce the risk of cardiovascular events by inhibiting the production of cholesterol in the liver",
     expirationDate: _date,
     price: 32500,
-    popularity: 520,
     image: AppProductsImages.lipitor,
     inStock: 21,
     isFavourate: false,
-    discount: 50,
   );
+  // ignore: non_constant_identifier_names
   static final Product Lantus = Product(
     id: 6,
-    type: ProductType.DiabetesManagement,
+    category: AppData.categories[6],
     name: "Lantus",
     scientificName: "Insulin glargine",
     brand: "Sanofi",
@@ -112,15 +107,14 @@ class Products {
         "Lantus is a long-acting insulin analogue used to manage diabetes by helping control blood sugar levels throughout the day and night",
     expirationDate: _date,
     price: 38500,
-    popularity: 520,
     image: AppProductsImages.lantus,
     inStock: 15,
     isFavourate: false,
-    discount: 50,
   );
+  // ignore: non_constant_identifier_names
   static final Product Cortizone = Product(
     id: 7,
-    type: ProductType.DermatologicalProducts,
+    category: AppData.categories[5],
     name: "Cortizone",
     scientificName: "Hydrocortisone",
     brand: "Bayer",
@@ -128,15 +122,14 @@ class Products {
         "Cortizone is a topical corticosteroid that helps relieve itching, redness, and inflammation associated with various skin conditions, providing effective relief for skin irritation",
     expirationDate: _date,
     price: 26500,
-    popularity: 520,
     image: AppProductsImages.cortizone,
     inStock: 86,
     isFavourate: false,
-    discount: 50,
   );
+  // ignore: non_constant_identifier_names
   static final Product Centrum = Product(
     id: 8,
-    type: ProductType.VitaminsAndSupplements,
+    category: AppData.categories[9],
     name: "Centrum",
     scientificName: "Multivitamins",
     brand: "Pfizer",
@@ -144,15 +137,14 @@ class Products {
         "Centrum is a comprehensive multivitamin supplement that supports overall health by providing essential vitamins and minerals, promoting vitality and well-being",
     expirationDate: _date,
     price: 31500,
-    popularity: 520,
     image: AppProductsImages.centrum,
     inStock: 49,
     isFavourate: false,
-    discount: 50,
   );
+  // ignore: non_constant_identifier_names
   static final Product OrthoTriCyclen = Product(
     id: 9,
-    type: ProductType.WomenHealth,
+    category: AppData.categories[10],
     name: "Ortho Tri-Cyclen",
     scientificName: "Ethinyl estradiol",
     brand: "Janssen Pharmaceuticals",
@@ -160,11 +152,9 @@ class Products {
         "Ortho Tri-Cyclen is an oral contraceptive (birth control pill) that combines different hormones to prevent ovulation and alter the cervical mucus and uterine lining, providing effective contraception for women",
     expirationDate: _date,
     price: 52000,
-    popularity: 520,
     image: AppProductsImages.orthoTriCyclen,
     inStock: 26,
     isFavourate: false,
-    discount: 50,
   );
 }
 
@@ -183,17 +173,17 @@ class AppData {
     Products.OrthoTriCyclen
   ];
   static List<Category> categories = [
-    Category(id: 0, name: "All", image: ""),
-    Category(id: 1, name: ProductType.Antibiotics, image: ""),
-    Category(id: 2, name: ProductType.AllergyMedications, image: ""),
-    Category(id: 3, name: ProductType.CardiovascularMedications, image: ""),
-    Category(id: 4, name: ProductType.ColdAndFlu, image: ""),
-    Category(id: 5, name: ProductType.DermatologicalProducts, image: ""),
-    Category(id: 6, name: ProductType.DiabetesManagement, image: ""),
-    Category(id: 7, name: ProductType.DigestiveHealth, image: ""),
-    Category(id: 8, name: ProductType.PainAndRelief, image: ""),
-    Category(id: 9, name: ProductType.VitaminsAndSupplements, image: ""),
-    Category(id: 10, name: ProductType.WomenHealth, image: ""),
+    Category(id: 0, name: "All"),
+    Category(id: 1, name: ProductType.Antibiotics),
+    Category(id: 2, name: ProductType.AllergyMedications),
+    Category(id: 3, name: ProductType.CardiovascularMedications),
+    Category(id: 4, name: ProductType.ColdAndFlu),
+    Category(id: 5, name: ProductType.DermatologicalProducts),
+    Category(id: 6, name: ProductType.DiabetesManagement),
+    Category(id: 7, name: ProductType.DigestiveHealth),
+    Category(id: 8, name: ProductType.PainAndRelief),
+    Category(id: 9, name: ProductType.VitaminsAndSupplements),
+    Category(id: 10, name: ProductType.WomenHealth),
   ];
   static List<List<Product>> filteredProducts = [
     products,
