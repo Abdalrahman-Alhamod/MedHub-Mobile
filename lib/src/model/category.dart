3e17f8e3f9a08d0948553c45371e510493b3c857
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-12-03 01:35:33 +0300

    create toString-fromJson-toJson-fromListJson-toListJson in category

--- lib/src/model/category.dart
+++ lib/src/model/category.dart
@@ -1,10 +1,41 @@
 class Category {
   int id;
   String name;
-  String image;
   Category({
     required this.id,
     required this.name,
-    required this.image,
   });
+  @override
+  String toString() {
+    return '''
+    Category {
+        id: $id,
+        name: $name,
+      }
+    ''';
+  }
+
+  // JSON encoder
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+    };
+  }
+
+  static List<Map<String, dynamic>> toJsonList(List<Category> categories) {
+    return categories.map((category) => category.toJson()).toList();
+  }
+
+  static List<Category> fromJsonList(List<Map<String, dynamic>> jsonList) {
+    return jsonList.map((json) => Category.fromJson(json)).toList();
+  }
+
+  // JSON decoder
+  factory Category.fromJson(Map<String, dynamic> json) {
+    return Category(
+      id: json['id'] as int,
+      name: json['name'] as String,
+    );
+  }
 }
