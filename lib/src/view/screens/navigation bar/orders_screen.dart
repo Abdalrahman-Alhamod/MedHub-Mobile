c73e6f4f328b82be0dd8d7e87fac89e22411628d
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-12-13 23:15:49 +0300

    implement orders screen UI with orders cubit apply api link

--- lib/src/view/screens/navigation bar/orders_screen.dart
+++ lib/src/view/screens/navigation bar/orders_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pharmacy_warehouse_store_mobile/core/assets/app_images.dart';
+import 'package:pharmacy_warehouse_store_mobile/core/constants/app_colors.dart';
+import 'package:pharmacy_warehouse_store_mobile/src/Cubits/Orders/orders_cubit.dart';
+import 'package:pharmacy_warehouse_store_mobile/src/model/order.dart';
+import 'package:pharmacy_warehouse_store_mobile/src/view/helpers/show_snack_bar.dart';
+import 'package:pharmacy_warehouse_store_mobile/src/view/widgets/order_card.dart';
 import 'package:pharmacy_warehouse_store_mobile/src/view/widgets/show_image.dart';
 
 class OrdersScreen extends StatelessWidget {
@@ -7,14 +13,79 @@ class OrdersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: ShowImage(
-          imagePath: AppImages.emptyOrders,
-          height: 600,
-          width: 600,
-        ),
+    BlocProvider.of<OrdersCubit>(context).getOrders();
+    return Scaffold(
+      body: BlocConsumer<OrdersCubit, OrdersState>(
+        listener: (context, state) {
+          if (state is OrdersFetchFailure) {
+            showSnackBar(state.errorMessage, SnackBarMessageType.error);
+          } else if (state is OrdersNetworkFailure) {
+            showSnackBar(state.errorMessage, SnackBarMessageType.error);
+          }
+        },
+        builder: (context, state) {
+          if (state is OrdersFetchSuccess) {
+            List<Order> orders = state.orders;
+            return _OrdersSuccessView(orders: orders);
+          } else if (state is OrdersFetchLoading) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: AppColors.primaryColor,
+              ),
+            );
+          } else if (state is OrdersFetchEmpty) {
+            return const Center(
+              child: ShowImage(
+                imagePath: AppImages.emptyOrders,
+                height: 500,
+                width: 500,
+              ),
+            );
+          } else if (state is OrdersFetchFailure) {
+            return const Center(
+              child: ShowImage(
+                imagePath: AppImages.error,
+                height: 500,
+                width: 500,
+              ),
+            );
+          } else if (state is OrdersNetworkFailure) {
+            return const Center(
+              child: ShowImage(
+                imagePath: AppImages.error404,
+                height: 500,
+                width: 500,
+              ),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(
+              color: AppColors.primaryColor,
+            ),
+          );
+        },
       ),
+    );
+  }
+}
+
+class _OrdersSuccessView extends StatelessWidget {
+  const _OrdersSuccessView({required this.orders});
+  final List<Order> orders;
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+      itemCount: orders.length,
+      scrollDirection: Axis.vertical,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 16.0),
+          child: OrderCard(
+            order: orders[index],
+          ),
+        );
+      },
     );
   }
 }
