e0810733ae4c823e205b1f142621b2a4b556eee8
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-12-13 23:10:35 +0300

    add new images paths to AppImages

--- lib/core/assets/app_images.dart
+++ lib/core/assets/app_images.dart
@@ -14,6 +14,15 @@ class AppImages {
   static const error404 = 'assets/images/error404.jpg';
   static const emptyCart = 'assets/images/emptyCart.png';
   static const emptyOrders = 'assets/images/emptyOrders.jpg';
+  static const allOrders = 'assets/images/allOrders.png';
+  static const orderBill = 'assets/images/orderBill.png';
+  static const orderDate = 'assets/images/orderDate.png';
+  static const orderDelivering = 'assets/images/orderDelivering.png';
+  static const orderID = 'assets/images/orderID.png';
+  static const orderPreparing = 'assets/images/orderPreparing.png';
+  static const orderRecieved = 'assets/images/orderRecieved.png';
+  static const orderRefused = 'assets/images/orderRefused.png';
+  static const orderStatus = 'assets/images/orderStatus.png';
   static const allImages = [
     startWallpaper,
     categoryWallpaper,
@@ -21,6 +30,14 @@ class AppImages {
     error,
     error404,
     emptyCart,
-    emptyOrders
+    emptyOrders,
+    orderBill,
+    orderDate,
+    orderDelivering,
+    orderID,
+    orderPreparing,
+    orderRecieved,
+    orderRefused,
+    orderStatus,
   ];
 }
