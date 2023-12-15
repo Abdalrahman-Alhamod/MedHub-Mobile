a46e34eb468dccb0cd85c64dfcbf750318d02add
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-12-16 01:59:05 +0300

    apply changes to home screen replace logout with profile remove bloc listener from app bar

--- lib/src/view/screens/navigation bar/home_screen.dart
+++ lib/src/view/screens/navigation bar/home_screen.dart
@@ -3,13 +3,9 @@ import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 import 'package:pharmacy_warehouse_store_mobile/core/constants/app_colors.dart';
-import 'package:pharmacy_warehouse_store_mobile/src/Cubits/Auth/Logout/logout_cubit.dart';
 import 'package:pharmacy_warehouse_store_mobile/src/Cubits/BottomNavBar/bottom_nav_bar_cubit.dart';
-import 'package:pharmacy_warehouse_store_mobile/src/Cubits/Orders/orders_cubit.dart';
 import 'package:pharmacy_warehouse_store_mobile/src/Locale/local_controller.dart';
-import 'package:pharmacy_warehouse_store_mobile/src/view/helpers/show_loading_dialog.dart';
-import 'package:pharmacy_warehouse_store_mobile/src/view/helpers/show_snack_bar.dart';
-import 'package:pharmacy_warehouse_store_mobile/src/view/screens/auth/login_screen.dart';
+import 'package:pharmacy_warehouse_store_mobile/src/view/screens/drawer/profile_screen.dart';
 import 'package:pharmacy_warehouse_store_mobile/src/view/screens/navigation bar/cart_screen.dart';
 import 'package:pharmacy_warehouse_store_mobile/src/view/screens/navigation bar/favourite_screen.dart';
 import 'package:pharmacy_warehouse_store_mobile/src/view/screens/navigation bar/orders_screen.dart';
@@ -37,7 +33,7 @@ class HomeScreenState extends State<HomeScreen> {
     return BlocBuilder<BottomNavBarCubit, BottomNavBarState>(
       builder: (context, state) {
         return Scaffold(
-          appBar: const CustomAppBar(),
+          appBar: const _HomeAppBar(),
           body: SizedBox.expand(
             child: HomeScreen
                 .screen[BlocProvider.of<BottomNavBarCubit>(context).index],
@@ -105,61 +101,46 @@ class HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const CustomAppBar({Key? key}) : super(key: key);
+class _HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const _HomeAppBar({Key? key}) : super(key: key);
 
   @override
   Size get preferredSize => const Size.fromHeight(56.0);
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<LogoutCubit, LogoutState>(
-      listener: (context, state) {
-        if (state is LogoutLoading) {
-          showLoadingDialog();
-        } else if (state is LogoutSuccess) {
-          Get.until((route) => !Get.isDialogOpen!);
-          showSnackBar("logedOutSuccess".tr, SnackBarMessageType.success);
-          Get.off(() => LoginScreen());
-        } else if (state is LogoutFailure) {
-          Get.until((route) => !Get.isDialogOpen!);
-          showSnackBar(state.errorMessage, SnackBarMessageType.error);
-        }
-      },
-      child: AppBar(
-        backgroundColor: AppColors.primaryColor,
-        title: const Text(
-          'MedHub',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
+    return AppBar(
+      backgroundColor: AppColors.primaryColor,
+      title: const Text(
+        'MedHub',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
         ),
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () async {
-            BlocProvider.of<LogoutCubit>(context).logout();
-          },
-          icon: const Icon(
-            Icons.logout,
-            size: 32,
-            color: AppColors.secondaryColor,
-          ),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              AppLocalController().toggleLang();
-              // BlocProvider.of<OrdersCubit>(context).getOrders();
-            },
-            icon: Icon(
-              Icons.translate,
-              size: 32,
-              color: Colors.grey.shade300,
-            ),
-          ),
-        ],
       ),
+      centerTitle: true,
+      leading: IconButton(
+        onPressed: () async {
+          Get.off(() => const ProfileScreen());
+        },
+        icon: const Icon(
+          Icons.account_circle,
+          size: 40,
+          color: Colors.white,
+        ),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () async {
+            AppLocalController().toggleLang();
+          },
+          icon: Icon(
+            Icons.language,
+            size: 40,
+            color: Colors.grey.shade300,
+          ),
+        ),
+      ],
     );
   }
 }
