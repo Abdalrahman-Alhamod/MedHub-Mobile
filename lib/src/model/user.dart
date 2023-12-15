d8fb48d8536d5ad0c7b7c7f3e89b2b4fdbc82894
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-12-16 01:57:46 +0300

    fix user model fix fromJson and toJson and test it add toString method

--- lib/src/model/user.dart
+++ lib/src/model/user.dart
@@ -2,10 +2,10 @@ class User {
   int id;
   String name;
   String pharmacyName;
-  String pharmacyLocation;
+  String? pharmacyLocation;
   int phoneNumber;
-  String password;
-  bool isAdmin;
+  String? password;
+  bool? isAdmin;
   String image;
   static String? token =
       "5BBzrJ2GCfeBB3RIHYBL2mEO3epUXQ3NfRVGoyX1hnHvV5RzhUAwErrkzUl5";
@@ -15,32 +15,45 @@ class User {
     required this.pharmacyName,
     required this.pharmacyLocation,
     required this.phoneNumber,
-    required this.password,
-    required this.isAdmin,
+    this.password,
+    this.isAdmin,
     required this.image,
   });
   factory User.fromJson(jsonData) {
     return User(
-        id: jsonData['id'],
-        name: jsonData['name'],
-        pharmacyName: jsonData['pharmacyName'],
+        id: jsonData['id'] as int,
+        name: jsonData['name'] as String,
+        pharmacyName: jsonData['pharmacyName'] as String,
         pharmacyLocation: jsonData['pharmacyLocation'],
-        phoneNumber: jsonData['phoneNumber'],
-        password: jsonData['password'],
-        isAdmin: jsonData['isAdmin'],
+        phoneNumber: int.parse(jsonData['phoneNumber']),
         image: jsonData['image']);
   }
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['id'] = id;
-
     data['name'] = name;
     data['pharmacyName'] = pharmacyName;
-    data['pharmacyLocation'] = pharmacyLocation;
+    data['pharmacyLocation'] = pharmacyLocation ?? "";
     data['phoneNumber'] = phoneNumber;
-    data['password'] = password;
-    data['isAdmin'] = isAdmin;
+    data['password'] = password ?? "";
+    data['isAdmin'] = isAdmin ?? false;
     data['image'] = image;
     return data;
+  }
+
+  @override
+  String toString() {
+    return '''
+    User {
+      id: $id,
+      name: $name,
+      pharmacyName: $pharmacyName,
+      pharmacyLocation: $pharmacyLocation,
+      phoneNumber: $phoneNumber,
+      password: $password,
+      isAdmin: $isAdmin,
+      image: $image
+    }
+  ''';
   }
 }
