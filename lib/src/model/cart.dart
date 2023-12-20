7fef414f12d7345ff1a85442c4ab5b8d3c244a0f
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-12-21 02:01:06 +0300

    delete unused models

--- lib/src/model/cart.dart
+++
@@ -1,8 +0,0 @@
-import 'package:pharmacy_warehouse_store_mobile/src/model/product.dart';
-
-class Cart {
-  int id;
-  double totalPrice = 0;
-  List<Product> products = <Product>[];
-  Cart({required this.id});
-}
--- lib/src/model/notification.dart
+++
@@ -1,8 +0,0 @@
-class Notification {
-  String title;
-  String description;
-  Notification({
-    required this.title,
-    required this.description,
-  });
-}
