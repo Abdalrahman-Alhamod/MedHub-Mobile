07a3aa8065189bd280f2ce55142c3b5f785e5bd6
Abdalrahman-Alhamod <abd.alrrahman.alhamod@gmail.com>
2023-12-31 06:45:47 +0300

    fix deleted files imports errors

--- lib/src/routes/app_pages.dart
+++ lib/src/routes/app_pages.dart
@@ -1,6 +1,5 @@
 import 'package:get/get.dart';
 import 'package:pharmacy_warehouse_store_mobile/src/routes/app_routes.dart';
-import 'package:pharmacy_warehouse_store_mobile/src/view/screens/app%20bar/notification_screen.dart';
 import 'package:pharmacy_warehouse_store_mobile/src/view/screens/auth/login_screen.dart';
 import 'package:pharmacy_warehouse_store_mobile/src/view/screens/auth/register_screen.dart';
 import 'package:pharmacy_warehouse_store_mobile/src/view/screens/drawer/profile_screen.dart';
@@ -9,7 +8,6 @@ import 'package:pharmacy_warehouse_store_mobile/src/view/screens/navigation%20ba
 import 'package:pharmacy_warehouse_store_mobile/src/view/screens/order_details_screen.dart';
 import 'package:pharmacy_warehouse_store_mobile/src/view/screens/product_details_screen.dart';
 import 'package:pharmacy_warehouse_store_mobile/src/view/screens/start/splash_screen.dart';
-import 'package:pharmacy_warehouse_store_mobile/src/view/screens/start/welcome_screen.dart';
 
 class AppPages {
   const AppPages._();
@@ -17,7 +15,6 @@ class AppPages {
   static const INITIAL = Routes.SPLASH;
   static final routes = [
     GetPage(name: Routes.SPLASH, page: () => const SplashScreen()),
-    GetPage(name: Routes.WELCOME, page: () => const WelcomeScreen()),
     GetPage(name: Routes.LOGIN, page: () => LoginScreen()),
     GetPage(name: Routes.REGISTER, page: () => RegisterScreen()),
     GetPage(name: Routes.HOME, page: () => const HomeScreen()),
@@ -25,7 +22,6 @@ class AppPages {
       name: Routes.PRODUCT_DETAILS,
       page: () => ProductDetailsScreen(),
     ),
-    GetPage(name: Routes.NOTIFICATIONS, page: () => const NotificationScreen()),
     GetPage(name: Routes.PROFILE, page: () => const ProfileScreen()),
     GetPage(name: Routes.STATISTICS, page: () => const StatisticsScreen()),
     GetPage(name: Routes.ORDER_DETAILS, page: () => const OrderDetailsScreen()),
